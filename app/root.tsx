eb0efe7865e013329c854f1b231e3443cb28288d
Praveen Bisht <prvnbist@gmail.com>
2022-11-03 23:26:42 +0530

    feat: supabase auth setup

--- app/root.tsx
+++ app/root.tsx
@@ -1,13 +1,100 @@
-import type { MetaFunction } from '@remix-run/node'
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
+import { useEffect, useState } from 'react'
+import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
+import { json, LoaderFunction, MetaFunction } from '@remix-run/node'
+import { createServerClient, createBrowserClient, SupabaseClient, Session } from '@supabase/auth-helpers-remix'
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData, useNavigate } from '@remix-run/react'
+
+import { Database } from '../db_types'
+
+type UserType = {
+   id: string
+   email: string
+   created: string
+   user_id: string
+}
+
+export type ContextType = {
+   supabase: SupabaseClient<Database> | null
+   session: Session | null
+   user: UserType | null
+}
+
+type LoaderData = {
+   env: { SUPABASE_URL: string; SUPABASE_ANON_KEY: string }
+   initialSession: Session | null
+}
 
 export const meta: MetaFunction = () => ({
    charset: 'utf-8',
-   title: 'New Remix App',
+   title: 'Hackernews Clone',
    viewport: 'width=device-width,initial-scale=1',
 })
 
+export const loader: LoaderFunction = async ({ request }) => {
+   // environment variables may be stored somewhere other than
+   // `process.env` in runtimes other than node
+   // we need to pipe these Supabase environment variables to the browser
+   const { SUPABASE_URL, SUPABASE_ANON_KEY } = process.env
+
+   // We can retrieve the session on the server and hand it to the client.
+   // This is used to make sure the session is available immediately upon rendering
+   const response = new Response()
+   const supabaseClient = createServerClient<Database>(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!, { request, response })
+   const {
+      data: { session: initialSession },
+   } = await supabaseClient.auth.getSession()
+
+   // in order for the set-cookie header to be set,
+   // headers must be returned as part of the loader response
+   return json(
+      {
+         initialSession,
+         env: {
+            SUPABASE_URL,
+            SUPABASE_ANON_KEY,
+         },
+      },
+      {
+         headers: response.headers,
+      }
+   )
+}
+
 export default function App() {
+   const navigate = useNavigate()
+   const [user, setUser] = useState<UserType | null>(null)
+   const { env, initialSession } = useLoaderData<LoaderData>()
+   const [supabase, setSupabase] = useState<SupabaseClient | null>(null)
+   const [session, setSession] = useState<Session | null>(initialSession)
+
+   const context: ContextType = { supabase, session, user }
+
+   useEffect(() => {
+      if (!supabase) {
+         const supabaseClient = createBrowserClient<Database>(env.SUPABASE_URL, env.SUPABASE_ANON_KEY)
+         setSupabase(supabaseClient)
+         const {
+            data: { subscription },
+         } = supabaseClient.auth.onAuthStateChange((_, session) => setSession(session))
+         return () => {
+            subscription.unsubscribe()
+         }
+      }
+   }, [])
+
+   useEffect(() => {
+      const get_user = async () => {
+         if (!supabase) return
+         const { data = [] } = await supabase.from('account').select('*').eq('user_id', session?.user?.id)
+         if (data?.length) {
+            const [user] = data
+            setUser(user?.id ? user : null)
+         }
+      }
+
+      if (session) get_user()
+   }, [session, supabase])
+
    return (
       <html lang="en">
          <head>
@@ -15,7 +102,20 @@ export default function App() {
             <Links />
          </head>
          <body>
-            <Outlet />
+            {session && (
+               <button
+                  onClick={async () => {
+                     await supabase?.auth.signOut()
+                     navigate('/')
+                  }}
+               >
+                  Logout
+               </button>
+            )}
+            {supabase && !session && (
+               <Auth redirectTo="http://localhost:3004" appearance={{ theme: ThemeSupa }} supabaseClient={supabase} socialLayout="horizontal" />
+            )}
+            <Outlet context={context} />
             <ScrollRestoration />
             <Scripts />
             <LiveReload />
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,24 +1,13 @@
+import { useOutletContext } from '@remix-run/react'
+
+import type { ContextType } from '../root'
+
 export default function Index() {
+   const { user, session } = useOutletContext<ContextType>()
+
    return (
       <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-         <h1>Welcome to Remix</h1>
-         <ul>
-            <li>
-               <a target="_blank" href="https://remix.run/tutorials/blog" rel="noreferrer">
-                  15m Quickstart Blog Tutorial
-               </a>
-            </li>
-            <li>
-               <a target="_blank" href="https://remix.run/tutorials/jokes" rel="noreferrer">
-                  Deep Dive Jokes App Tutorial
-               </a>
-            </li>
-            <li>
-               <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-                  Remix Docs
-               </a>
-            </li>
-         </ul>
+         <pre>{JSON.stringify({ user, session }, null, 2)}</pre>
       </div>
    )
 }
